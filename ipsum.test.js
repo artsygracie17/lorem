73105fcc65ad9138c9d3a082bfe54217a105e93b
gracie <gfang@wellesley.edu>
2018-11-08 01:22:18 -0500

    Add mock functions

--- ipsum.test.js
+++ ipsum.test.js
@@ -3,10 +3,20 @@ const parallel = ipsum.parallel
 const series = ipsum.series
 const cb = () => {}
 
+const mocks = require('./ipsum_mock')
+const { exampleArray } = mocks
+
+
 describe('Parallel Tests', () => {
   test('Empty array', () => {
     const mockFn = jest.fn()
     parallel([mockFn], cb)
     expect(mockFn).toHaveBeenCalled()
+  })
+
+  test('Example array', () => {
+    const mockFn = jest.fn()
+    parallel(exampleArray, cb)
+    expect(exampleArray).toHaveBeenCalledTimes(9)
   })
 })
